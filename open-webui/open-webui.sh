ee9f264d44320f8f43d395df910715b02c6b6763
Ashwin Rao <ashwin.rao@helsinki.fi>
2025-09-11 14:34:42 +0300

    Changes

--- open-webui/open-webui.sh
+++ open-webui/open-webui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#set -x
+# set -x
 
 POSITIONAL_ARGS=()
 SCRIPT_NAME=$0
@@ -13,6 +13,9 @@ PORT_NUM=${DEFAULT_PORT}
 DOCKER_CMD='/usr/bin/docker'
 CONTAINER_NAME="open-webui"
 CONTAINER_IMAGE="ghcr.io/open-webui/open-webui:main"
+OLLAMA_URL="http://192.168.8.1:8080"
+
+
 
 function display_help() {
     echo ${SCRIPT_NAME}
@@ -24,7 +27,12 @@ function stop_container() {
   echo "Stopping ${CONTAINER_NAME}"
   docker stop ${CONTAINER_NAME}
   echo "Removing ${CONAINER_NAME}"
+  sleep 1
   docker rm ${CONTAINER_NAME}
+}
+
+function start_browser() {
+  (sleep 3;  xdg-open localhost:${PORT_NUM} > /dev/null 2>&1) & > /dev/null 2>&1
 }
 
 # parse the arguments
@@ -54,6 +62,14 @@ then
     exit 0
 fi
 
+# Check if it already running
+if docker ps -a | grep -q "${CONTAINER_NAME}"; then
+  start_browser
+  echo "Container ${CONTAINER_NAME} is already running."
+  exit 1
+fi   
+
+# Now do the steps to spawn the container. 
 
 if [ ${PORT_NUM} -lt 1024 ] || [ ${PORT_NUM} -gt 65000 ]
 then
@@ -66,11 +82,11 @@ fi
 echo "Spawning ${CONTAINER_NAME} container using the following arguments"
 echo "Port number: "${PORT_NUM}
 
-(sleep 3;  xdg-open localhost:${PORT_NUM} > /dev/null 2>&1) & > /dev/null 2>&1
+start_browser
 
-${DOCKER_CMD} run --rm \
+${DOCKER_CMD} run --rm -d \
               --name ${CONTAINER_NAME} \
               -p ${PORT_NUM}:${PORT_NUM} \
-              -e OLLAMA_BASE_URL=http://192.168.8.1:8080 \
+              -e OLLAMA_BASE_URL="${OLLAMA_URL}"\
               -v open-webui:/app/backend/data \
               ${CONTAINER_IMAGE}
